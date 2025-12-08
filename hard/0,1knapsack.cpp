bd2f2345291dee4782db76a8c93df49fc5f692db
rauthanv <rauthanvandana0@gmail.com>
2025-12-08 13:24:06 +0530

    0/1knapsack Memoization

--- hard/0,1knapsack.cpp
+++ hard/0,1knapsack.cpp
@@ -1,19 +1,27 @@
-class Solution
-{
-public:
-    int knapsack(int W, vector<int> &val, vector<int> &wt)
-    {
-        int n = (int)val.size();
-        return knapsackRec(W, val, wt, n);
+class Solution {
+  public:
+    int t[1001][1001];
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        memset(t, -1, sizeof(t)); 
+        return knapsackRec(W, val, wt, val.size());
     }
-    int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n)
-    {
-        if (n == 0 || W == 0)
-            return 0;
-        if (wt[n - 1] <= W)
-        {
-            return max(val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1), knapsackRec(W, val, wt, n - 1));
+    
+    //recursive 2^n
+    int knapsackRec(int W,vector<int> &val, vector<int> &wt,int n){
+        if(n==0||W==0) return 0;
+        if(wt[n-1]<=W){
+            return max(val[n-1] + knapsackRec(W-wt[n-1],val,wt,n-1), knapsackRec(W,val,wt,n-1));
         }
-        return knapsackRec(W, val, wt, n - 1);
+        return knapsackRec(W,val,wt,n-1);
+    }
+
+    // memoization(optimized) o(n*W)
+    int knapsackRec(int W,vector<int> &val, vector<int> &wt,int n){
+        if(n==0||W==0) return 0;
+        if(t[n][W]!=-1) return t[n][W];
+        if(wt[n-1]<=W){
+            return t[n][W]=max(val[n-1] + knapsackRec(W-wt[n-1],val,wt,n-1), knapsackRec(W,val,wt,n-1));
+        }
+        return t[n][W]=knapsackRec(W,val,wt,n-1);
     }
 };
