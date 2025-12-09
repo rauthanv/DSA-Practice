22c3f9d355e374f4437865c79d21f5e9ee3722c4
rauthanv <rauthanvandana0@gmail.com>
2025-12-10 00:08:25 +0530

    Partition Eql Sum

---
+++ hard/partitionEqlSubsetSm.cpp
@@ -0,0 +1,36 @@
+// Problem: Partition Equal Subset Sum
+
+class Solution {
+public:
+
+    // ------------------ TABULATION FUNCTION ------------------ //
+    bool subsetSum(vector<int>& nums, int sum) {
+        int n = nums.size();
+        bool t[n + 1][sum + 1];
+
+        for(int i = 0; i <= n; i++) t[i][0] = true;
+
+        for(int j = 1; j <= sum; j++) t[0][j] = false;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= sum; j++) {
+                if(nums[i - 1] <= j) {
+                    t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+        return t[n][sum];
+    }
+
+    // ------------------ MAIN FUNCTION ------------------ //
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+        if(sum % 2 != 0) return false;
+        return subsetSum(nums, sum / 2);
+    }
+};
