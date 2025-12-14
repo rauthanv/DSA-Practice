60f8ae97536288d227bef21b542c410d4161727c
rauthanv <rauthanvandana0@gmail.com>
2025-12-15 00:09:38 +0530

    Matrix addition

--- easy/matrixAddition.cpp
+++ easy/matrixAddition.cpp
@@ -1,13 +1,10 @@
 // Problem: Matrix Addition
 
-
 class Solution {
 public:
     void Addition(vector<vector<int>>& matrixA,vector<vector<int>>& matrixB) {
-
-        int rows = matrixA.size();
-        int cols = matrixA[0].size();
-
+        int row = matrixA.size();
+        int col = matrixA[0].size();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 matrixA[i][j] = matrixA[i][j] + matrixB[i][j];
